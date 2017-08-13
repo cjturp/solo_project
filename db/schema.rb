d24597fa62f8dc94ba92dde7203daf1f5f3faa93
Christopher Turpin <cjturpin1@gmail.com>
2017-08-12 22:16:46 -0400

    added paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170813015732) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pictures", force: :cascade do |t|
     t.string "title"
     t.string "url"
